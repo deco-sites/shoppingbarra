1ffedc0379eef8f59f6a94e03a365d945f6c479b
SACCI <saccilucas@gmail.com>
2023-07-01 21:19:08 -0300

    fix(footer): conflitos resolvidos

--- components/footer/SecondaryFooter.tsx
+++ components/footer/SecondaryFooter.tsx
@@ -1,16 +1,14 @@
-const getYear = () => {
-  const date = new Date();
-  return date.getFullYear();
-};
+export interface Props {
+  text: string;
+}
 
-function SecondaryFooter() {
-  const year = getYear();
+function SecondaryFooter({ text }: Props) {
   return (
     <footer
-      class={`w-full flex flex-col pt-5 pb-5 bg-[#ff6c14] text-white text-sm`}
+      class={`w-full flex flex-col pt-5 pb-5 bg-primary text-white text-sm`}
     >
       <div class="flex flex-col lg:flex-row lg:justify-center max-w-full text-center">
-        Copyright © {year}. Todos os direitos reservados.
+        {text}
       </div>
     </footer>
   );
