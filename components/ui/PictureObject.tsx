f64865d89739cc3d46a97674139a1c6fe5583706
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2023-07-04 10:53:19 -0300

    feat: improve mobile performance

--- components/ui/PictureObject.tsx
+++ components/ui/PictureObject.tsx
@@ -1,6 +1,6 @@
+import Icon from "$store/components/ui/Icon.tsx";
+import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
-import Image from "deco-sites/std/components/Image.tsx";
-import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
 
 export interface Content {
   label: string;
@@ -17,16 +17,29 @@ export interface Props {
 export default function PictureObject({ content, highlight }: Props) {
   return (
     <>
-      <Image
-        src={content.image}
-        alt={content.imageAlt}
-        decoding="async"
-        loading="lazy"
-        class="rounded-xl object-cover w-full"
-        {...(highlight
-          ? { width: 1200, height: 840 }
-          : { width: 358, height: 177 })}
-      />
+      <Picture>
+        {highlight && (
+          <Source
+            media="(max-width: 767px)"
+            src={content.image}
+            width={358}
+            height={177}
+            class="h-auto"
+          />
+        )}
+        <Source
+          media={highlight ? "(min-width: 768px)" : ""}
+          src={content.image}
+          {...(highlight
+            ? { width: 1200, height: 840 }
+            : { width: 358, height: 177 })}
+        />
+        <img
+          class="rounded-xl object-cover w-full"
+          src={content.image}
+          alt={content.imageAlt}
+        />
+      </Picture>
       <div class="flex justify-between items-center">
         <div class="text-xs lg:text-base">
           {content.label}
