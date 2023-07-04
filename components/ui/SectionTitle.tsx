6a94bcb362128fafacb7e677e2f64658e1190dec
aline__santos <aline.santos@partner.sdslasupport.com>
2023-07-03 21:42:14 -0300

    feat: final adjusts banner text component

--- components/ui/SectionTitle.tsx
+++ components/ui/SectionTitle.tsx
@@ -1,41 +1,39 @@
 interface Props {
-    title?: string;
-    fontSize?: "Normal" | "Large";
-    alignment: "center" | "left";
-    colorReverse?: boolean;
-  }
-  
-  function SectionTitle(props: Props) {
-    return (
-      <>
-        {props.title
-          ? (
-            <div
-              class={`flex flex-col gap-2 py-10 ${
-                props.alignment === "left" ? "text-left" : "text-center"
-              }`}
-            >
-              {props.title &&
-                (
-                  <h1
-                    class={`text-3xl leading-8 lg:leading-10 font-bold
-                    ${
-                      props.colorReverse
-                        ? "text-primary-content"
-                        : "text-base-content"
-                    }
-                    ${props.fontSize === "Normal" ? "lg:text-3xl" : "lg:text-4xl"}
+  title?: string;
+  textColor?: SemanticColors;
+}
+
+export type SemanticColors =
+  | "primary"
+  | "base"
+  | "accent";
+
+function SectionTitle(props: Props) {
+  const CONTENT_COLORS = {
+    primary: "text-primary group-hover:text-primary-content",
+    base: "text-base group-hover:text-base-content",
+    accent: "text-accent group-hover:text-accent-content",
+  };
+  return (
+    <>
+      {props.title
+        ? (
+          <div class="flex flex-col gap-2 py-8 lg:py-10 text-center">
+            {props.title &&
+              (
+                <h1
+                  class={`text-4xl leading-8 lg:leading-10 font-bold
+                  ${CONTENT_COLORS[props.textColor ?? "accent"]}
                   `}
-                  >
-                    {props.title}
-                  </h1>
-                )}
-            </div>
-          )
-          : null}
-      </>
-    );
-  }
-  
-  export default SectionTitle;
-  
+                >
+                  {props.title}
+                </h1>
+              )}
+          </div>
+        )
+        : null}
+    </>
+  );
+}
+
+export default SectionTitle;
--- sections/Content/BannerWithText.tsx
+++ sections/Content/BannerWithText.tsx
@@ -4,132 +4,115 @@ import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 import Button from "$store/components/ui/Button.tsx";
 import SectionTitle from "$store/components/ui/SectionTitle.tsx";
 
-export interface Banner {
+export interface BannerText {
+  /** @description Banner image */
   src: LiveImage;
-  /**
-   * @description Image alt text
-   */
+  /** @description Image's alt text */
   alt: string;
+  /** @description title on the top of the text content*/
+  bannerTitle: string;
+  /**@description Text Content*/
+  text: HTML;
+  /**@description Button Text*/
+  buttonText?: string;
+  /**@description Button href*/
+  href?: string;
+  /**@description Button Background Color
+    @default primary
+  */
+  background?: SemanticColors;
+
+  hideShowButton?: 'show' | 'hide'
 }
 
 export interface Props {
-  /**
-    @description Page Title
-    @default Title
-  */
   title?: string;
-
-  banners?: Banner[];
-
+  bannerTextContent: BannerText[];
+  /**@description Section Title */
+  textColor?: SemanticColors;
   /**
-    @description Banner Title
-    @default BannerTitle
+    @description Position of the banner and text
+    @default bannerRowPositioning
   */
-  bannerTitle: string;
-
-  /**
-    @description Banner Text Content
-    @default Text 
-  */
-  text: HTML;
-
-  /**
-    @description Button Text
-    @default Button
-  */
-  buttonText: string;
-
-  /**
-    @description Button href
-    @default /
-  */
-  href?: string;
-
-  /**
-    @description Page Title Alignment
-    @default center
-  */
-  alignment?: "center" | "left";
-
-  /**
-    @description Page Title Font Size
-    @default Large
-  */
-  fontSize?: "Normal" | "Large";
-
-  /**
-    @description Button Background Color
-    @default primary
-  */
-  buttonColor?: SemanticColors;
+  contentPosition?: "bannerRowPositioning" | "bannerRowReversePositioning";
 }
 
 export type SemanticColors =
   | "primary"
-  | "secondary"
-  | "base";
+  | "base"
+  | "accent";
 
 export default function BannerWithText({
   title,
-  banners = [],
-  bannerTitle,
-  text,
-  buttonText,
-  href,
-  alignment,
-  fontSize,
-  buttonColor
+  bannerTextContent = [],
+  textColor,
+  contentPosition,
 }: Props) {
-
   const BTN_COLORS = {
     primary: "btn btn-primary border-primary-content",
-    secondary: "btn btn-secondary border-secondary-content",
     base: "btn btn-base border-base-content",
+    accent: "text-accent group-hover:text-accent-content",
   };
+
+  const contentDirection = {
+    bannerRowPositioning: "md:flex-row",
+    bannerRowReversePositioning: "md:flex-row-reverse",
+  };
+
+  const hideButton = {
+    show: 'show',
+    hide: 'hidden'
+  }
 
   return (
     <>
       <SectionTitle
         title={title || ""}
-        fontSize={fontSize || "Normal"}
-        alignment={alignment || "center"}
+        textColor={textColor}
       />
       <section>
-        <div class="container py-10 flex flex-col md:flex-row gap-8 lg:gap-10 justify-center px-2">
-          <div>
-            {banners.map(({ src, alt }) => (
-              <Image
-                src={src}
-                alt={alt}
-                width={400}
-                height={500}
-                preload
-                loading="eager"
-                fetchPriority="high"
-              />
-            ))}
-          </div>
+        <div
+          class={`container lg:pt-10 flex flex-col gap-8 lg:gap-10 justify-center px-4 mb-11 ${
+            contentDirection[contentPosition ?? "bannerRowPositioning"]
+          }`}
+        >
+          {bannerTextContent.map((
+            { src, alt, bannerTitle, text, buttonText, href, background, hideShowButton },
+          ) => (
+            <>
+              <div>
+                <Image
+                  src={src}
+                  alt={alt}
+                  width={400}
+                  height={500}
+                  preload
+                  loading="eager"
+                  fetchPriority="high"
+                />
+              </div>
 
-          <div>
-            <strong
-              class="text-2xl text-center"
-              style="color: rgb(100, 48, 100)"
-            >
-              {bannerTitle}
-            </strong>
-            <div
-              dangerouslySetInnerHTML={{ __html: text }}
-            />
-            <a href={href}>
-              <Button
-                class={`text-primary-content text-base rounded-3xl normal-case mt-10 ${BTN_COLORS[
-                  buttonColor ?? "primary"
-                ]}`}
-              >
-                {buttonText}
-              </Button>
-            </a>
-          </div>
+              <div>
+                <h2 class="text-2xl text-center font-bold pb-4 text-accent uppercase">
+                  {bannerTitle}
+                </h2>
+                <div
+                  dangerouslySetInnerHTML={{ __html: text }}
+                />
+                <a href={href} class="flex justify-center">
+                  <Button
+                    class={`text-base rounded-3xl normal-case mt-10 font-normal ${
+                      BTN_COLORS[
+                        background ?? "primary"
+                      ]
+                    } ${hideButton[hideShowButton ?? 'show']}`}
+                  >
+                    {buttonText}
+                  </Button>
+                </a>
+              </div>
+            </>
+          ))}
         </div>
       </section>
     </>
